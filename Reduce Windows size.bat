8065d936c90c7143c82efc43940aca6c638a8b4c
SecretlySpy <professor6x@gmail.com>
2021-04-26 05:32:01 +0800

    Additional Tweaks

---
+++ Reduce Windows size.bat
@@ -0,0 +1,13 @@
+@echo off
+title Optimize Windows for better performance&cls&echo ============================================================================&echo # Optimize Windows for better performance (Disable dmwappushservice + Enabling the Compact OS feature in Windows 10&echo ============================================================================
+Compact /CompactOS:always
+compact /c /s /a /i /exe:lzx %programFiles(x86)%\*
+compact /c /s /a /i /exe:lzx %programFiles%
+compact /c /s /a /i /exe:lzx "C:\ProgramData\*"
+compact /c /s /a /i /exe:lzx "C:\Users\*"
+compact /c /s /a /i /exe:lzx %windir%\Assembly\*
+compact /c /s /a /i /exe:lzx %windir%\InfusedApps\*
+compact /c /s /a /i /exe:lzx %windir%\Panther\*
+compact /c /s /a /i /exe:lzx %windir%\SoftwareDistribution\*
+compact /c /s /a /i /exe:lzx %windir%\System32\Catroot2\*
+compact /c /s /a /i /exe:lzx %windir%\System32\LogFiles\*
